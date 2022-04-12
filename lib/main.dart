7548dcd8ac9b4a18afd34293db54638d9d2b569e
Satyam Gupta <satyamx811@gmail.com>
2022-04-12 21:25:54 +0530

    day1 completed

--- lib/main.dart
+++ lib/main.dart
@@ -17,15 +17,52 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           leading: IconButton(
+            tooltip: 'menu',
             icon: const Icon(Icons.menu),
             onPressed: (){},
             ),
-          title: Text('Firts app'),
+            actions: [
+              IconButton(
+                padding: const EdgeInsets.only(right: 25),
+                tooltip: 'search',
+                onPressed: (){},
+                icon: const Icon(Icons.search),
+                )
+            ],
+            
+          title: const Text('Firts app'),
         ),
         body: Center(
-          child: Text('Hello world!'),
+          child: GestureDetector(
+            onTap: (){
+              print('my button was tab');
+            },
+            child: Container(
+              height: 50,
+              padding: const EdgeInsets.all(8.0),
+              margin: const EdgeInsets.symmetric(horizontal: 8.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25.0),
+                color: Colors.green
+              ),
+              child: const Center(
+                child: Text(
+                  'Click here'
+                  ),
+                  ),
+            ),
+          ),
         ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){},
+          child: IconButton(
+            tooltip: 'Add',
+            icon: Icon(Icons.add),
+            onPressed: (){},),
+          ),
+      
         ),
+        
   
     );
   }
