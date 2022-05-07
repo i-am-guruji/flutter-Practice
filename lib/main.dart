447b016ecd12d44a37b06c5aad30eacc77659841
Satyam Gupta <satyamx811@gmail.com>
2022-05-07 19:27:24 +0530

    Route transfer animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,66 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
+
 
 void main(){
 
-  runApp(const MyApp());
+  runApp(
+    const MaterialApp(
+      home: Page1(),
+    )
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+class Page1 extends StatelessWidget {
+  const Page1({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('List view'),
-        ) ,
-        body: ListView(
-          
-          children: [
-            _tile('CineArts At  the  Empire', '85 W Portal  Ave', Icons.theaters,Icons.send),
-            _tile('The Castro Theater', '429 Castro St', Icons.theaters,Icons.send),
-            _tile('Alamo Drafthouse Cinema', '2550 Mission St', Icons.theaters,Icons.send),
-            _tile('Roxie Theater', '3117 16th St', Icons.theaters,Icons.send),
-            _tile('United Artists Stonestown Twin', '501 Buckingham Way',Icons.theaters,Icons.send),
-            _tile('AMC Metreon 16', '135 4th St #3000', Icons.theaters,Icons.send),
-            const Divider(color: Colors.black,),
-            _tile('K\'s Kitchen', '757 Monterey Blvd', Icons.restaurant,Icons.send),
-            _tile('Emmy\'s Restaurant', '1923 Ocean Ave', Icons.restaurant,Icons.send),
-            _tile('Chaiya Thai Restaurant', '272 Claremont Blvd', Icons.restaurant,Icons.send),
-            _tile('La Ciccia', '291 30th St', Icons.restaurant,Icons.send),
-            
-
-
-          ],
-
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: (){
+            Navigator.of(context).push(_createRoute());
+          },
+          child: Text("Go!"),
         ),
-        ),
-      
+      ),    
     );
   }
 }
 
-ListTile _tile(String title,String subtitle,IconData icon,IconData icons){
+Route _createRoute(){
+  return PageRouteBuilder(
+    pageBuilder: (context, animation,secondaryAnimation) => const Page2(),
+    transitionsBuilder: (context, animation,secondaryAnimation,child){
+      const begin = Offset(0.0, 0.1);
+      const end = Offset.zero;
+      const curve = Curves.ease;
 
-  return ListTile(
-    onTap: (){},
-    title: Text(title,style: TextStyle(
-      fontSize: 20,
-      fontWeight: FontWeight.w500
-    ),),
-    subtitle: Text(subtitle),
-    leading: Icon(icon,color: Colors.blue[500],),
-    trailing: Icon(icons,color: Colors.blue[500],),
+      var tween = Tween(begin: begin,end: end).chain(CurveTween(curve: curve));
 
-  );
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+        );
+    }
+
+    );
+}
+
+class Page2 extends StatelessWidget {
+  const Page2({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Text("Page2"),
+      ),
+      
+    );
+  }
 }
